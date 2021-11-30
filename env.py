09fff290dbe59bb0dbefcf6fc8538baa7f9e5b16
CarlssonOscar <oosccar@gmail.com>
2021-11-30 12:26:24 +0000

    a

---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ['DATABASE_URL'] = 'postgres://aubntvpzujdrvu:eca5a17ca22b56031f9b3f735eb3fc6e3e83dbc10ef4c4dc74eae2b323a4e175@ec2-54-195-246-55.eu-west-1.compute.amazonaws.com:5432/dbkau7cflhiil4'
+os.environ['SECRET_KEY'] = 'randomSecretKey007%%'
+os.environ["CLOUDINARY_URL"] = 'cloudinary://494923639249566:F_0fS0RoFiCbDXBbevmE-Yu3-MU@dqllg16xq'
