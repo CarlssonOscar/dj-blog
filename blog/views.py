96e6b342e824a93d820810e5ee99026a0fa576b9
CarlssonOscar <oosccar@gmail.com>
2021-12-01 11:27:21 +0000

    Add starter templates

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
-# Create your views here.
+class PostList(generic.ListView):
+    model = Post
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+    # Means paginate just means separate into pages.  
+    # By setting paginate_by to six, we're limiting the  number of
+    #  posts that can appear on the front page, if there are more
+    #  than six then Django  will automatically add page navigation
+    paginate_by = 6
