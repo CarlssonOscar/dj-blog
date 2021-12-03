3bcddb163d949cda9f780a49889c12ab1d39bf25
CarlssonOscar <oosccar@gmail.com>
2021-12-03 07:05:23 +0000

    a

--- blog/views.py
+++ blog/views.py
@@ -33,6 +33,42 @@ class PostDetail(View):
             {
                 'post': post,
                 'comments': comments,
+                'commented': False,
+                'liked': liked,
+                'comment_form': CommentForm()
+            }
+        )
+
+    def post(self, request, slug, *args, **kwargs):
+        # First, we'll filter all of our posts so that
+        # we  only have the active ones with status set to one.
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by('-created_on')
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+        
+        # Will get all data that is posted from our form
+        comment_form = CommentForm(data=request.POST)
+
+        # If valid a comment has been submitted and needs to be processed.
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+        else :
+            comment_form = CommentForm()
+
+        return render (
+            request,
+            'post_detail.html',
+            {
+                'post': post,
+                'comments': comments,
+                'commented': True,
                 'liked': liked,
                 'comment_form': CommentForm()
             }
